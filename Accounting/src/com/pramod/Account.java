21b1968fb090ded11912375490dc598e834cb36d
Pramod kumar <pramodkumarp6@gmail.com>
2022-03-14 23:30:23 +0530

    Welcome

--- Accounting/src/com/pramod/Account.java
+++ Accounting/src/com/pramod/Account.java
@@ -1,9 +1,10 @@
+package com.pramod;
 
 public class Account {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+		System.out.println("Mother");
 		// TODO Auto-generated method stub
 
 	}
